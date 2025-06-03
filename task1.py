4c710aa78d9e37ba4bf4e0adb917cfdb0c6f0c14
Laoumos <n0tnam3dd@bk.ru>
2025-06-03 11:08:42 +0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,8 +1,7 @@
 # Задание 1. Посчитайте сумму целых чисел, расположенных между числами 1 и N
 # включительно;
 user_input = int(input('\t 1 + ... + N\nВведите число N: '))
-result = 0
-if user_input != 1:
-    for i in range(1, user_input + 1):
-        result += i
-print(f'Результат есть {result}')
+if user_input:
+    for i in range(1, user_input):4
+        user_input += i
+print(f'Результат есть {user_input}')
